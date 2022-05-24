4307297b11d1b349ffde684f50868b38df4df1b6
creasedigital <ojorechris@gmail.com>
2022-05-24 08:45:41 +0100

    form styling completed

--- src/components/UpdateTodo.js
+++ src/components/UpdateTodo.js
@@ -22,26 +22,35 @@ const UpdateTodo = ({ setTodos, todos }) => {
 	return (
 		<div>
 			<form onSubmit={handleSubmit} className="text-center mt-4">
-				<div>
-					<label htmlFor="name">Task</label>
+				<div className="mb-4">
+					<label htmlFor="name" className="text-cyan-900 mr-4 font-bold ">
+						Enter A Task:
+					</label>
 					<input
 						type="text"
 						id="name"
 						value={todoInfo.name}
 						onChange={handleChange}
-						className="bg-cyan-100 px-20 py-4 "
+						className="bg-cyan-100 px-20 py-4"
 					/>
 				</div>
-				<div>
-					<label htmlFor="desc">Task</label>
+				<div className="mb-4">
+					<label htmlFor="desc" className="text-cyan-900 mr-4 font-bold ">
+						Description:
+					</label>
 					<input
 						type="text"
 						id="desc"
 						value={todoInfo.desc}
 						onChange={handleChange}
+						className="bg-cyan-100 px-20 py-4"
 					/>
 				</div>
-				<button>ADD TODO</button>
+				<div className="mb-4 m-auto">
+					<button className="text-left bg-cyan-900 text-lg text-white px-20 py-4">
+						ADD TODO
+					</button>
+				</div>
 			</form>
 		</div>
 	);
