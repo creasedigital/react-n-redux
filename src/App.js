f865e9e8587d6675c12fe644f16c2c727fb8947b
creasedigital <ojorechris@gmail.com>
2022-05-25 00:33:59 +0100

    title margin adjusted

--- src/App.js
+++ src/App.js
@@ -21,10 +21,10 @@ function App() {
 	return (
 		<div className="w-screen min-h-screen bg-white dark:bg-slate-900 text-cyan-900  dark:text-slate-100">
 			<div className="text-5xl font-light mb-2 py-5 text-cyan-900  dark:text-slate-100 text-center">
-				<span className="text-5xl font-bold mb-2 py-5 text-cyan-600  dark:text-cyan-400 text-center">
+				<span className="text-5xl font-bold mb-2 mr-1 py-5 text-cyan-600  dark:text-cyan-400 text-center">
 					&#10003;
 				</span>
-				TODO APP
+				TODO
 			</div>
 			<UpdateTodo todos={todos} setTodos={setTodos} removeTodo={removeTodo} />
 			<TodoContainer
